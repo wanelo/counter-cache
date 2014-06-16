b83e31f2257488ad9ccc70e602669292584bf886
Matt Camuto & Paul Henry <matt.camuto+paul@wanelo.com>
2014-06-16 14:11:14 -0700

    Stall all the counters.

---
+++ lib/counter/stall.rb
@@ -0,0 +1,7 @@
+require "counter/stall/version"
+
+module Counter
+  module Stall
+    # Your code goes here...
+  end
+end
---
+++ lib/counter/stall/version.rb
@@ -0,0 +1,5 @@
+module Counter
+  module Stall
+    VERSION = "0.0.1"
+  end
+end
